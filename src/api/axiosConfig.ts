8e681c9682ee84ca13a2f81d0e1f056661b61e03
d4ve07979 <davekenkou11@gmail.com>
2026-03-22 01:03:15 +0100

    augmenter timeout 120s

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8097
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 30000, // 30 secondes au lieu de 10
+  timeout: 120000, // 120 secondes au lieu de 10
   withCredentials: false,
   xsrfCookieName: 'XSRF-TOKEN',
   xsrfHeaderName: 'X-XSRF-TOKEN',
